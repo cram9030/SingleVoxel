8a03afa603cae38b1cadc06e259fe50f520bef0d
cram9030 <ncramer@ucsc.edu>
2016-12-02 18:20:08 -0800

    Added solver framework
    The current itteration does not work because of the observer. I am not
    completely sure why

---
+++ DecentralizedLMI.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [subL,subK,subG,subZ,K,L] = DecentralizedLMI(subA,subD,subB,subH,subM,subW,subV,subI_c,subO_c)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DecentralizedLMI: Computes the decralized LMI matrices	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function calculated the observer and controller gain matrices for
+% the decentralized system that is given
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	Author:	Nick Cramer, UCSC, Department of Computer Engineering			
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Called by:    --------------------										
+% Calls:	MATLAB 5.2 std fcns
+% Inputs:   subA - State Matrix Structure
+%           subD - Disturbance Matrix Structure
+%           subB - Input Matrix Structure
+%           subH - Control State Matrix Structure
+%           subM - Output Matrix Structure
+%           subW - Disturbance Noise Matrix Structure
+%           subV - Sensor Noise Matrix Structure
+%           subI_c - Input Covariance Weighting Matrix Structure
+%           subO_c - Output Covariance Weighting Matrix Structure
+% Outputs:  subL - Observer Gain Structure
+%           subK - Controller Gain Structure
+%           subG - G Strcture
+%           subZ - Z Structure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Intialize Strcutres
+subL(1).L = [];
+subG(1).G = [];
+subZ(1).Z = [];
+subK(1).K = [];
+
+tildeLength = 0;
+for i = 1:length(subA)
+    tildeLength = tildeLength+length(subA(i).A);
+end
+K = zeros(length(subA),tildeLength-length(subA)+2);
+L = zeros(tildeLength-length(subA)+2,length(subA));
+
+%Iterate through each LMI design criteria
+pos = 1;
+for i = 1:length(subA)
+    [subL(i).L,subG(i).G,subZ(i).Z] = LMIDesign(subA(i).A,subD(i).D,subB(i).B,subH(i).H,subM(i).M,subW(i).W,subV(i).V,subI_c(i).I_c,subO_c(i).O_c);
+    subK(i).K = subG(i).G/subZ(i).Z;
+    K(i,pos:length(subK(i).K)+pos-2) = subK(i).K(1:end-1);
+    K(i,end-10+i) = subK(i).K(end);
+    L(pos:length(subL(i).L)+pos-2,i) = subL(i).L(1:end-1);
+    L(end-10+i,i) = subL(i).L(end);
+    pos = pos + length(subK(i).K)-3;
+end
---
+++ controlledVoxel.m
@@ -0,0 +1,49 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dx = controlledVoxel(t,x)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% controlledVoxel: 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FILL IN
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	Author:	Nick Cramer, UCSC, Department of Computer Engineering			
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Called by:    --------------------										
+% Calls:	MATLAB 5.2 std fcns
+% Inputs:   t - time
+%           x - state values
+%               The first group of states 1:(length(x))/2 are the actual
+%               states. The second group
+%               length(x)/2+1:end-m are the "observed" states for the
+%               controller. 
+% Outputs:  dx - derivates of input state x
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Notes: the normal input u is actually part of the states because the
+%           state matrix has been augmented by the control u. This causes
+%           the u refered to here to be u = udot
+
+% Set Global Vairables
+global Aaug Baug Maug Daug K L
+
+[~,n] = size(Daug);
+
+% Splint the states
+x_a = x(1:length(x)/2);
+x_c = x(length(x)/2+1:end);
+
+% Calculate U and the actual state vector
+u = -K*x_c;
+u = zeros(size(u));
+u = K*x_a;
+
+if t<0.0001
+    w = [zeros(n-1,1);1];
+else
+    w = zeros(n,1);
+end
+dx_a = Aaug*x_a+Baug*u+Daug*w;
+
+% Calcuate the observed/estimated state vector
+dx_c = Aaug*x_c + Baug*u + L*(Maug*x_a-Maug*x_c);
+
+% Combine derivates
+dx = [dx_a;dx_c];
